3d74cecc6ad572bf171d3307be73a5ec3112bc44
BorisykVova <36400106+BorisykVova@users.noreply.github.com>
2019-01-26 22:55:48 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_POR = int(os.environ.get('MAIL_POR') or 25)
+    MAIL_USER_TLS = os.environ.get('MAIL_USER_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['doterua66@gmail.com']
+    POSTS_PER_PAGE = 3
